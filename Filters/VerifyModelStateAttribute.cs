705d33fe11cb211bfb31e917e4636f399d0eee93
Hjorthen <Hjorthensebastian@gmail.com>
2017-11-04 13:24:07 +0100

    Added filter to return BadRequest errors when ModelState is invalid

---
+++ Filters/VerifyModelStateAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Linq;
+using System;
+
+namespace VCAPI.Filters
+{
+    public class VerifyModelStateAttribute : Attribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            // Do nothing
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if(!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState.Values.SelectMany(s => s.Errors).Select(v => v.ErrorMessage));
+            }
+        }
+    }
+}
