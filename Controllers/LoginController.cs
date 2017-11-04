29b5a5cef5b3ba365d3a042ca8653a0c7a7342a0
Hjorthen <Hjorthensebastian@gmail.com>
2017-11-04 15:16:52 +0100

    Added user creation

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,39 +1,73 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
+using VCAPI.Filters;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System;
+using System.Text;
+using VCAPI.Repository.Interfaces;
 
 namespace VCAPI.Controllers
 {
     [Route("api/")]
     public class LoginController : Controller
     {
-        public class RegisterCredentials{
+        private readonly Encoding encoding;
+        private readonly IUserRepository repo;
+
+        public LoginController(IUserRepository repo)
+        {
+            this.repo = repo;
+            encoding = Encoding.UTF8; 
+        }
+
+        public struct RegisterCredentials{
             [Required]
-            [MinLength(1)]
-            public string username;
+            public string username { get; set; }
             [Required]
-            public string password;
+            public string password { get; set; }
             [Required]
-            public string CASCODE;
+            public string CASCODE { get; set; }
         }
 
         public class LoginCredentials{
             [Required]
-            public string username;
+            public string username { get; set; }
             [Required]
-            public string password;
+            public string password { get; set; }
         }
 
         [HttpPost("signup")]
-        public IActionResult Signup([FromBody] RegisterCredentials credentials){
-            if(!ModelState.IsValid)
+        [VerifyModelState]
+        public async Task<IActionResult> Signup([FromBody] RegisterCredentials credentials){
+            HttpClient client = new HttpClient();
+            HttpRequestMessage msg = new HttpRequestMessage()
             {
-                return BadRequest(ModelState.Values);
+                Method = HttpMethod.Get,
+                RequestUri = new System.Uri("http://www.campusnet.dtu.dk/data/CurrentUser/Userinfo")
+            };
+            msg.Headers.Add("x-appname", "Opslagsystem for økobil");
+            msg.Headers.Add("x-token", "3ddfc095-5a62-4162-a058-5bc3784e36d7");
+            string token = Convert.ToBase64String(encoding.GetBytes(String.Format("{0}:{1}", credentials.username, credentials.CASCODE)));
+          //  string token = encoding.GetString(Convert.FromBase64String(String.Format("{0}:{1}", credentials.username, credentials.CASCODE)));
+            msg.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", token);
+            HttpResponseMessage result = await client.SendAsync(msg);
+            if (result.IsSuccessStatusCode)
+            {
+                // Create user
+                return Ok();
             }
-            return Ok(credentials);
+            else
+            {
+                return BadRequest("Failed to verify login with campusnet");
+            }
+
         }
 
         [HttpPut("login")]
-        public IActionResult Login(){
+        public IActionResult Login([FromBody]LoginCredentials credentials)
+        {
+            // Lookup user in DB
             return Ok();
         }
     }
