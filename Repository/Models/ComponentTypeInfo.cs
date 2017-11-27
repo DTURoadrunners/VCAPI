1617f616f050e75b7cf0a0c942b7a41825a1e890
Cecilie <cille112@yahoo.dk>
2017-11-27 09:33:03 +0100

    log

---
+++ Repository/Models/ComponentTypeInfo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VCAPI.Repository.Models
+{
+    public class ComponentTypeInfo
+    {
+        public int id;
+        public string name;
+        public int categoryID;
+        public int storage;
+        public string description;
+    }
+}
